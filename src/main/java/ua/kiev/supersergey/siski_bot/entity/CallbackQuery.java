a69b1fb872d95144e6e27e37a56ed3879ec58b32
supersergey <sergey.tolokunsky@gmail.com>
2016-12-02 16:21:17 +0200

    small fixes

---
+++ src/main/java/ua/kiev/supersergey/siski_bot/entity/CallbackQuery.java
@@ -0,0 +1,58 @@
+package ua.kiev.supersergey.siski_bot.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Created by sergey on 30.11.2016.
+ */
+public class CallbackQuery {
+    private String id;
+    private User from;
+    private Message message;
+    private String data;
+    @JsonProperty("inline_message_id")
+    private String inlineMessageId;
+
+    public CallbackQuery() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public User getFrom() {
+        return from;
+    }
+
+    public void setFrom(User from) {
+        this.from = from;
+    }
+
+    public Message getMessage() {
+        return message;
+    }
+
+    public void setMessage(Message message) {
+        this.message = message;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getInlineMessageId() {
+        return inlineMessageId;
+    }
+
+    public void setInlineMessageId(String inlineMessageId) {
+        this.inlineMessageId = inlineMessageId;
+    }
+}
